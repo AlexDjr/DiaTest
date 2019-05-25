6e21c24ccd49d0dc5fc3288dd74d18c45c9167dc
Alex Delin <fcbalexd@gmail.com>
2019-05-25 20:42:45 +0300

    Refactor LoginViewController

--- DiaTest/Controllers/LoginViewController.m
+++ DiaTest/Controllers/LoginViewController.m
@@ -26,75 +26,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self setupWebView];
+    [self setupView];
 }
-
 
 #pragma mark - UIWebViewDelegate
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *requestString = request.URL.description;
+    const BOOL isRequestContainAccessToken = [requestString rangeOfString:@"#access_token="].location != NSNotFound;
     
-    //    проверяем, что полученный URL это ответ, содержащий в себе access_token
-    if ([[[request URL] description] rangeOfString:@"#access_token="].location != NSNotFound) {
-        AccessToken *token = [AccessToken new];
-        
-        //    делаем парсинг полученного ответа
-        NSString *query = [[request URL] description];
-        //    разбиваем строку на части по знаку #. Части складываем в массив
-        NSArray *array = [query componentsSeparatedByString:@"#"];
-        //    получаем часть после знака #
-        if ([array count] > 1) {
-            query = [array lastObject];
-        }
-        
-        //    разбиваем строку на части по знаку &. Части складываем в массив
-        NSArray *pairs = [query componentsSeparatedByString:@"&"];
-        for (NSString *pair in pairs) {
-            //    разбиваем строку на части по знаку =. Части складываем в массив
-            NSArray *values = [pair componentsSeparatedByString:@"="];
-            //    получаем значения ключа и значения
-            if ([values count] == 2) {
-                NSString *key = [values firstObject];
-                //    если это access_token, то заполняем свойства нашего объекта token
-                if ([key isEqualToString:@"access_token"]) {
-                    token.token = [values lastObject];
-                } else if ([key isEqualToString:@"expires_in"]) {
-                    NSTimeInterval interval = [[values lastObject] doubleValue];
-                    token.expirationDate = [NSDate dateWithTimeIntervalSinceNow:interval];
-                } else if ([key isEqualToString:@"user_id"]) {
-                    token.userId = [values lastObject];
-                }
-            }
-        }
+    if (isRequestContainAccessToken) {
+        NSArray *accessTokenAttrPairs = [self obtainAccessTokenAttrPairsFromRequest:requestString];
+        AccessToken *accessToken = [self accessTokenFromAttrPairs:accessTokenAttrPairs];
         
         if (self.completion) {
-            self.completion(token);
+            self.completion(accessToken);
         }
         
         [self dismissViewControllerAnimated:YES completion:nil];
-        
         return NO;
     }
-    
     return YES;
 }
 
 #pragma mark - Methods
-- (void)setupWebView {
-    UIWebView *webView = [UIWebView new];
-    [self.view addSubview:webView];
+- (void)setupView {
+    [self setupNavBar];
+    [self setupCancelBarButton];
     
-    webView.translatesAutoresizingMaskIntoConstraints = NO;
-    [webView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
-    [webView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
-    [webView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
-    [webView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
-    
+    NSURLRequest * request = [self setupURLRequest];
+    [self setupWebViewWithRequest:request];
+}
+
+- (NSString *)setupNavBar {
+    return self.navigationItem.title = @"Авторизация";
+}
+
+- (void)setupCancelBarButton {
     UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(actionCancel:)];
     [self.navigationItem setRightBarButtonItem:item animated:NO];
-    
-    self.navigationItem.title = @"Авторизация";
-    
+}
+
+- (NSURLRequest *)setupURLRequest {
     NSString *urlString = [NSString stringWithFormat:
                            @"https://oauth.vk.com/authorize?"
                            "client_id=6984654&"
@@ -102,15 +74,58 @@
                            "redirect_uri=https://oauth.vk.com/blank.html&"
                            "scope=wall&"
                            "response_type=token&"
-//                           "revoke=1&"
                            "v=5.95"];
-    
     
     NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    return request;
+}
 
+- (void)setupWebViewWithRequest:(NSURLRequest *)request  {
+    UIWebView *webView = [UIWebView new];
     webView.delegate = self;
+    
+    [self.view addSubview:webView];
+    webView.translatesAutoresizingMaskIntoConstraints = NO;
+    [webView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+    [webView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+    [webView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
+    [webView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
+    
     [webView loadRequest:request];
+}
+
+- (NSArray *)obtainAccessTokenAttrPairsFromRequest:(NSString *)query {
+    NSString *accessTokenFullString = [[query componentsSeparatedByString:@"#"] lastObject];
+    NSArray *accessTokenAttrPairs = [accessTokenFullString componentsSeparatedByString:@"&"];
+    return accessTokenAttrPairs;
+}
+
+- (AccessToken *)accessTokenFromAttrPairs:(NSArray *)attrPairs {
+    AccessToken *accessToken = [AccessToken new];
+    for (NSString *attrPair in attrPairs) {
+        [self setupAccessToken:accessToken fromAttrPair:attrPair];
+    }
+    return accessToken;
+}
+
+- (void)setupAccessToken:(AccessToken *)accessToken fromAttrPair:(NSString *)attrPair {
+    NSArray *attributeComponents = [attrPair componentsSeparatedByString:@"="];
+    
+    const BOOL isCorrectAttrPair = [attributeComponents count] == 2;
+    
+    if (isCorrectAttrPair) {
+        NSString *attributeKey = [attributeComponents firstObject];
+        NSString *attributeValue = [attributeComponents lastObject];
+        
+        if ([attributeKey isEqualToString:@"access_token"]) {
+            accessToken.token = attributeValue;
+        } else if ([attributeKey isEqualToString:@"expires_in"]) {
+            accessToken.expirationDate = [NSDate dateWithTimeIntervalSinceNow:[attributeValue doubleValue]];
+        } else if ([attributeKey isEqualToString:@"user_id"]) {
+            accessToken.userId = attributeValue;
+        }
+    }
 }
 
 #pragma mark - Actions
@@ -118,7 +133,6 @@
     if (self.completion) {
         self.completion(nil);
     }
-    
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
